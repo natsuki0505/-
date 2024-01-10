c2e5c11833766cb6c49df219b80958c73c8a7173
natsuki-kodani <153087863+natsuki0505@users.noreply.github.com>
2024-01-10 11:24:31 +0900

    CSS,JS フォルダのアップ

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// ナビここから
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+// topへ戻るボタン JS ↓
+$(document).ready(function() {
+  var pagetop = $('.pagetop');
+  console.log('ここまでいけてます')
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+            }
+       });
+       pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+              return false;
+   });
+});
+// topへ戻るボタン JS ↑
+
+
+  
+
---
+++ js/slick.js
@@ -0,0 +1,32 @@
+// animal-intro_desc
+$(function () {
+    $('#js-slider-3').slick({
+      arrows: true, // 前・次のボタンを表示する
+      dots: true, // ドットナビゲーションを表示する
+      appendDots: $('.dots-3'), // ドットナビゲーションの生成位置を変更
+      speed: 1000, // スライドさせるスピード（ミリ秒）
+      slidesToShow: 1, // 表示させるスライド数
+      centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+      variableWidth: true, // スライド幅の自動計算を無効化
+    });
+
+    $('#js-slider-4').slick({
+      arrows: true, // 前・次のボタンを表示する
+      dots: true, // ドットナビゲーションを表示する
+      appendDots: $('.dots-3'), // ドットナビゲーションの生成位置を変更
+      speed: 1000, // スライドさせるスピード（ミリ秒）
+      slidesToShow: 1, // 表示させるスライド数
+      centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+      variableWidth: true, // スライド幅の自動計算を無効化
+    });
+
+    $('#js-slider-5').slick({
+      arrows: true, // 前・次のボタンを表示する
+      dots: true, // ドットナビゲーションを表示する
+      appendDots: $('.dots-3'), // ドットナビゲーションの生成位置を変更
+      speed: 1000, // スライドさせるスピード（ミリ秒）
+      slidesToShow: 1, // 表示させるスライド数
+      centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+      variableWidth: true, // スライド幅の自動計算を無効化
+    });
+  });
